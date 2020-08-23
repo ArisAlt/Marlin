b5def0bb70c037562ce7dec6c1dcddb44e9d7094
ArisAlt <54513458+ArisAlt@users.noreply.github.com>
2020-08-23 13:47:04 +0300

    2 step motors

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -993,7 +993,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -0.1 }
+#define NOZZLE_TO_PROBE_OFFSET { 33, 5, -1 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
