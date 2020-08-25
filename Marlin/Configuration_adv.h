c4b7c578187290dbb9d131dafa53ddb85ad51bab
ArisAlt <54513458+ArisAlt@users.noreply.github.com>
2020-08-26 01:48:29 +0300

    disable Z_STEPPER_ALIGN_XY

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -718,7 +718,7 @@
   // Define probe X and Y positions for Z1, Z2 [, Z3 [, Z4]]
   // If not defined, probe limits will be used.
   // Override with 'M422 S<index> X<pos> Y<pos>'
-  #define Z_STEPPER_ALIGN_XY { {  36, 122 }, { 240,  122 } }
+  //#define Z_STEPPER_ALIGN_XY { {  36, 122 }, { 240,  122 } }
 
   /**
    * Orientation for the automatically-calculated probe positions.
