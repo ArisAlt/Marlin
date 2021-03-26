73b61d9c61e16f7b9f3a47324eb0a63faab195ce
Aris <abaourakis@gmail.com>
2021-03-26 02:52:36 +0200

    Add_Optic_end_stop

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -472,7 +472,7 @@
 #define HEATER_5_MAXTEMP 275
 #define HEATER_6_MAXTEMP 275
 #define HEATER_7_MAXTEMP 275
-#define BED_MAXTEMP      110
+#define BED_MAXTEMP      130
 #define CHAMBER_MAXTEMP  60
 
 /**
